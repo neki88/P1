f4ff5218389fc7f5da517000d2cd232b5a091874
Beoneki <neki41@gmail.com>
2022-06-25 21:07:27 +0200

    jss

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 // osnovne komande https://teamtreehouse.com/library/display-messages-with-javascript
 
+// var
+
+var poruka = 'da li ovo radi?';
+console.log(poruka);
+poruka = 'menjam valu od var poyy';
+console.log(poruka);
+
+// let const
+
+// METHOD: prompt();  
+
+let odg1 = prompt('ko si ti bre');
+let odg2 = prompt('kako prezime lolo');
+alert("Dobar dan " +odg1 + " " +odg2);
+
+//String Concatenation better way
+
+const name = prompt('What is ur name?');
+const message = 'Hello ' + name;
+
+console.log(message);
